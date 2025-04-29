ca2759e9b90e85e7b964bebfd3a37004a33c3f8c
Ashita  D Jawali <113736983+Ashita2619@users.noreply.github.com>
2025-04-29 15:33:07 -0500

    Update WF_3_helper.py

--- scripts/WF_3_DB/WF_3_helper.py
+++ scripts/WF_3_DB/WF_3_helper.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0,'/home/ssh_user/Documents/GitHub/CRAB_Analysis/scripts')
+sys.path.insert(0,'/epi/home/ssh_user/Documents/GitHub/CRAB_Analysis/scripts')
 from ms_sql_handler import ms_sql_handler
 import pandas as pd
 import cx_Oracle as co
@@ -30,28 +30,19 @@ class demographics_import():
 
     
     def get_lims_demographics(self,hsn,date,csv_path): #1
-
         self.wgs_run_date = date[:2]+"/"+date[2:4]+"/20"+date[4:]
         unfound_hsn=[]
-        conn = co.connect(self.lims_connection)
-                
-        query="select * from crecrpa5demo where HSN in ("+",".join(hsn)+")"
-        
-        self.lims_df = pd.read_sql(query,conn)   
-        #print(self.lims_df.to_string())
-        #import exce file with demographics if needed
-        
-        #excel_df = pd.read_csv(csv_path+"/CRAB_2022.csv",na_values="NaN")
-        excel_df =pd.read_excel(csv_path+"/CRAB_2022.xlsx",sheet_name='Sheet1',converters={'HSN':int})
-        #print(excel_df.to_string())
-        found_hsn = [ str(i) for i in self.lims_df['HSN'].values.tolist() ]
+        conn = co.connect(self.lims_connection)      
+        query="select * from crecrpa5demo where HSN in (" + ",".join(hsn) + ")"
+        self.lims_df = pd.read_sql(query, conn)
+        excel_df =pd.read_excel(csv_path + "/HAI_Metadata.xlsx", sheet_name='Sheet1', converters={'HSN':int})
+        found_hsn = [str(i) for i in self.lims_df['HSN'].values.tolist() ]
         self.no_lims_hsn = pd.DataFrame(columns=excel_df.columns.values.tolist())
-
         for h in hsn: #if no demographical information add a blank line
             if h not in found_hsn :
                 print(str(h)+" not found in lims df")
                 try:
-                    r= excel_df.query("HSN == "+str(h))
+                    r= excel_df.query("HSN == " + str(h))
                     self.no_lims_hsn = pd.concat([r,self.no_lims_hsn])
                 except Exception as e:
                     print("This error was found: \t"+e)
@@ -59,32 +50,35 @@ class demographics_import():
                     print(str(h)+" not found in CSV file")
                     #hsn.remove(h)
                     unfound_hsn.append(h) 
-
-
-
         conn.close()
         print("Not found in LIMS or CSV")
         print(unfound_hsn)
         return hsn
-    
+
     def format_lims_df(self): #2
         # manipulate sql database to format accepted by the master EXCEL worksheet
         #self.log.write_log("format_lims_DF","Manipulating demographics to database format")
 
         self.lims_df = self.lims_df.rename(columns = self.demo_names)
         #print(self.lims_df.head().to_string())
-        
+        def safe_to_datetime(date_str):
+            try:
+                return pd.to_datetime(date_str)
+            except ValueError:
+                return pd.NaT
+
         self.no_lims_hsn = self.no_lims_hsn.loc[:, ~self.no_lims_hsn.columns.str.contains('^Unnamed')]
         self.no_lims_hsn['name']=self.no_lims_hsn['First Name'] + " " + self.no_lims_hsn['Last Name']
         self.no_lims_hsn['HSN']=self.no_lims_hsn['HSN'].astype(int)
         self.no_lims_hsn = self.no_lims_hsn.rename(columns={"Rec'd":"date_recd","WGS Rec'd": "pcr_run_date","HSN": "hsn","Collected": "doc","DOB": "dob","Sex": "sex","State": "state","Source Site": "source","CO":"county"})
         self.no_lims_hsn['pcr_run_date']= pd.to_datetime(self.no_lims_hsn['pcr_run_date'])
         self.no_lims_hsn['date_recd']= pd.to_datetime(self.no_lims_hsn['date_recd'])
-        self.no_lims_hsn['dob']= pd.to_datetime(self.no_lims_hsn['dob'])
+        self.no_lims_hsn['dob']= self.no_lims_hsn['dob'].apply(safe_to_datetime)
         self.no_lims_hsn['doc']= pd.to_datetime(self.no_lims_hsn['doc'])
         self.no_lims_hsn.drop(columns=['Extracted','Sequenced','Last Name','First Name','Age','Source Type','Country','Comment','WGS serotype','coverage (calculated from workbook)','#total reads','Clusters passing filter',"HAI WGS ID"], axis=1, inplace=True)
 
-        self.lims_df= pd.concat([self.lims_df,self.no_lims_hsn])
+        # if not self.no_lims_hsn.empty:
+        self.lims_df = pd.concat([self.lims_df, self.no_lims_hsn])
         #self.log.write_log("format_lims_DF","Done!")
 
 
@@ -94,8 +88,8 @@ class demographics_import():
 
         self.mlst_df = pd.DataFrame.from_dict(mlst_dict,orient='index',columns=['hsn','species','mlst'])
         self.mlst_df['hsn']=self.mlst_df['hsn'].astype(int)
-        #print(len(self.mlst_df))
-        #print(self.mlst_df.head())
+        # print(len(self.mlst_df))
+        # print(self.mlst_df.head())
 
     def create_metrics_df(self,assembly_m,pipeline):
         #res[sample]=sample_buso_res["results"]["one_line_summary"]
@@ -167,7 +161,7 @@ class demographics_import():
 
     def assign_HAI_ID(self,formatted_df,year,max_id,path_to_excel): #could alos be used to add in run metrics
         
-        run_metrics =pd.read_excel(path_to_excel+"/CRAB_2022.xlsx",sheet_name='Sheet1',converters={'HSN':int})
+        run_metrics =pd.read_excel(path_to_excel+"/HAI_Metadata.xlsx",sheet_name='Sheet1',converters={'HSN':int})
 
         for i,row in formatted_df.iterrows():
             max_id+=1
@@ -200,7 +194,7 @@ class demographics_import():
             HAI_ID = self.db_handler.sub_read(query="SELECT MAX(HAI_WGS_ID) AS MAX_ID FROM dbo.Results where wgs_run_date >= cast('"+str(self.wgs_run_date[-4:])+"' as datetime)")
             HAI_ID_MAX=int(HAI_ID.to_string()[-5:])
             #2023DG-00080
-            #2023DC00080
+            #2023DG00080
         except:
             print("No HAI ID")
             HAI_ID_MAX = 0
@@ -209,7 +203,7 @@ class demographics_import():
         self.df = self.assign_HAI_ID(self.df,self.wgs_run_date[-4:],HAI_ID_MAX,excel_path)
         #print(self.df.head())
         df_demo_lst = self.df.values.astype(str).tolist()
-       
+        # print(df_demo_lst[:5])  # Check the first few rows of data being pushed to SQL
         i=0
         while i < len(df_demo_lst):
         
@@ -222,39 +216,33 @@ class demographics_import():
         #df_table_col_query = "(" + ", ".join(self.df.columns.astype(str).tolist()) + ")"
         
         self.write_query_tbl1 = (" ").join(self.write_query_tbl1)
-
+        # print(self.write_query_tbl1)
         self.create_ncbi_csv(excel_path,runD,df_demo_lst)
 
-        self.db_handler.lst_ptr_push(df_lst=df_demo_lst, query=self.write_query_tbl1)
-        #self.log.write_log("database_push","Done!`")
-    
-    #will need a function for resistance table push
-    def database_push_genes(self):
-        #self.log.write_log("database_push","Starting")
-        self.setup_db()
-        df_demo_lst = self.genes_df.values.astype(str).tolist()
+        try:
+            self.db_handler.lst_ptr_push(df_lst=df_demo_lst, query=self.write_query_tbl1)
+        except Exception as e:
+            print(f"Error pushing data to SQL: {e}")
 
-        #df_demo_lst=[['2320191', "aph(3'')-Ib", '98.31', '99.88', 'ncbi', 'NG_056002.2', "aminoglycoside O-phosphotransferase APH(3'')-Ib", 'STREPTOMYCIN'], ['2320191', 'aph(6)-Id', '100.00', '100.00', 'ncbi', 'NG_047464.1', 'aminoglycoside O-phosphotransferase APH(6)-Id', 'STREPTOMYCIN'], ['2320191', 'tet(B)', '99.50', '100.00', 'ncbi', 'NG_048161.1', 'tetracycline efflux MFS transporter Tet(B)', 'TETRACYCLINE'], ['2320191', 'blaADC-30', '100.00', '100.00', 'ncbi', 'NG_048652.1', 'class C extended-spectrum beta-lactamase ADC-30', 'CEPHALOSPORIN'], ['2320191', 'blaOXA-66', '100.00', '100.00', 'ncbi', 'NG_049806.1', 'OXA-51 family carbapenem-hydrolyzing class D beta-lactamase OXA-66', 'CARBAPENEM'], ['2320191', 'blaOXA-72', '100.00', '100.00', 'ncbi', 'NG_049813.1', 'OXA-24 family carbapenem-hydrolyzing class D beta-lactamase OXA-72', 'CARBAPENEM'], ['2320191', 'msr(E)', '100.00', '100.00', 'ncbi', 'NG_048007.1', 'ABC-F type ribosomal protection protein Msr(E)', 'MACROLIDE'], ['2320191', 'mph(E)', '100.00', '100.00', 'ncbi', 'NG_064660.1', "Mph(E) family macrolide 2'-phosphotransferase", 'MACROLIDE'], ['2320191', "ant(3'')-IIa", '100.00', '98.61', 'ncbi', 'NG_054646.1', "aminoglycoside nucleotidyltransferase ANT(3'')-IIa", 'SPECTINOMYCIN;STREPTOMYCIN'], ['2320191', 'aac(3)-Ia', '100.00', '100.00', 'ncbi', 'NG_047234.1', 'aminoglycoside N-acetyltransferase AAC(3)-Ia', 'GENTAMICIN'], ['2320191', "aac(6')-Ip", '100.00', '99.66', 'ncbi', 'NG_047307.2', "aminoglycoside 6'-N-acetyltransferase AAC(6')-Ip", 'AMINOGLYCOSIDE'], ['2322433', "ant(3'')-IIa", '100.00', '98.61', 'ncbi', 'NG_054646.1', "aminoglycoside nucleotidyltransferase ANT(3'')-IIa", 'SPECTINOMYCIN;STREPTOMYCIN'], ['2322433', "aph(3'')-Ib", '98.31', '99.88', 'ncbi', 'NG_056002.2', "aminoglycoside O-phosphotransferase APH(3'')-Ib", 'STREPTOMYCIN'], ['2322433', 'aph(6)-Id', '100.00', '100.00', 'ncbi', 'NG_047464.1', 'aminoglycoside O-phosphotransferase APH(6)-Id', 'STREPTOMYCIN'], ['2322433', 'tet(B)', '99.50', '100.00', 'ncbi', 'NG_048161.1', 'tetracycline efflux MFS transporter Tet(B)', 'TETRACYCLINE'], ['2322433', 'blaADC-30', '100.00', '100.00', 'ncbi', 'NG_048652.1', 'class C extended-spectrum beta-lactamase ADC-30', 'CEPHALOSPORIN'], ['2322433', 'blaOXA-66', '100.00', '100.00', 'ncbi', 'NG_049806.1', 'OXA-51 family carbapenem-hydrolyzing class D beta-lactamase OXA-66', 'CARBAPENEM'], ['2322433', 'dfrA17', '100.00', '99.79', 'ncbi', 'NG_047710.1', 'trimethoprim-resistant dihydrofolate reductase DfrA17', 'TRIMETHOPRIM'], ['2322433', 'aadA5', '100.00', '100.00', 'ncbi', 'NG_047357.1', "ANT(3'')-Ia family aminoglycoside nucleotidyltransferase AadA5", 'STREPTOMYCIN'], ['2322433', 'sul1', '100.00', '100.00', 'ncbi', 'NG_048082.1', 'sulfonamide-resistant dihydropteroate synthase Sul1', 'SULFONAMIDE'], ['2322433', 'armA', '100.00', '100.00', 'ncbi', 'NG_047476.1', 'ArmA family 16S rRNA (guanine(1405)-N(7))-methyltransferase', 'GENTAMICIN'], ['2322433', 'msr(E)', '100.00', '100.00', 'ncbi', 'NG_048007.1', 'ABC-F type ribosomal protection protein Msr(E)', 'MACROLIDE'], ['2322433', 'mph(E)', '100.00', '100.00', 'ncbi', 'NG_064660.1', "Mph(E) family macrolide 2'-phosphotransferase", 'MACROLIDE'], ['2322433', 'aac(3)-Ia', '100.00', '100.00', 'ncbi', 'NG_047234.1', 'aminoglycoside N-acetyltransferase AAC(3)-Ia', 'GENTAMICIN'], ['2322433', 'sul2', '100.00', '100.00', 'ncbi', 'NG_051852.1', 'sulfonamide-resistant dihydropteroate synthase Sul2', 'SULFONAMIDE'], ['2322433', "aac(6')-Ip", '100.00', '99.66', 'ncbi', 'NG_047307.2', "aminoglycoside 6'-N-acetyltransferase AAC(6')-Ip", 'AMINOGLYCOSIDE'], ['2322433', 'blaOXA-72', '100.00', '100.00', 'ncbi', 'NG_049813.1', 'OXA-24 family carbapenem-hydrolyzing class D beta-lactamase OXA-72', 'CARBAPENEM'], ['2318415', 'blaADC-30', '100.00', '100.00', 'ncbi', 'NG_048652.1', 'class C extended-spectrum beta-lactamase ADC-30', 'CEPHALOSPORIN'], ['2318415', "ant(3'')-IIa", '100.00', '98.61', 'ncbi', 'NG_054646.1', "aminoglycoside nucleotidyltransferase ANT(3'')-IIa", 'SPECTINOMYCIN;STREPTOMYCIN'], ['2318415', 'tet(B)', '99.50', '100.00', 'ncbi', 'NG_048161.1', 'tetracycline efflux MFS transporter Tet(B)', 'TETRACYCLINE'], ['2318415', 'aph(6)-Id', '100.00', '100.00', 'ncbi', 'NG_047464.1', 'aminoglycoside O-phosphotransferase APH(6)-Id', 'STREPTOMYCIN'], ['2318415', "aph(3'')-Ib", '98.31', '99.88', 'ncbi', 'NG_056002.2', "aminoglycoside O-phosphotransferase APH(3'')-Ib", 'STREPTOMYCIN'], ['2318415', 'dfrA17', '100.00', '99.79', 'ncbi', 'NG_047710.1', 'trimethoprim-resistant dihydrofolate reductase DfrA17', 'TRIMETHOPRIM'], ['2318415', 'aadA5', '100.00', '100.00', 'ncbi', 'NG_047357.1', "ANT(3'')-Ia family aminoglycoside nucleotidyltransferase AadA5", 'STREPTOMYCIN'], ['2318415', 'sul1', '100.00', '100.00', 'ncbi', 'NG_048082.1', 'sulfonamide-resistant dihydropteroate synthase Sul1', 'SULFONAMIDE'], ['2318415', 'armA', '100.00', '100.00', 'ncbi', 'NG_047476.1', 'ArmA family 16S rRNA (guanine(1405)-N(7))-methyltransferase', 'GENTAMICIN'], ['2318415', 'msr(E)', '100.00', '100.00', 'ncbi', 'NG_048007.1', 'ABC-F type ribosomal protection protein Msr(E)', 'MACROLIDE'], ['2318415', 'mph(E)', '100.00', '100.00', 'ncbi', 'NG_064660.1', "Mph(E) family macrolide 2'-phosphotransferase", 'MACROLIDE'], ['2318415', 'aac(3)-Ia', '100.00', '100.00', 'ncbi', 'NG_047234.1', 'aminoglycoside N-acetyltransferase AAC(3)-Ia', 'GENTAMICIN'], ['2318415', 'sul2', '100.00', '100.00', 'ncbi', 'NG_051852.1', 'sulfonamide-resistant dihydropteroate synthase Sul2', 'SULFONAMIDE'], ['2318415', "aac(6')-Ip", '100.00', '99.66', 'ncbi', 'NG_047307.2', "aminoglycoside 6'-N-acetyltransferase AAC(6')-Ip", 'AMINOGLYCOSIDE'], ['2318415', 'blaOXA-72', '100.00', '100.00', 'ncbi', 'NG_049813.1', 'OXA-24 family carbapenem-hydrolyzing class D beta-lactamase OXA-72', 'CARBAPENEM'], ['2318415', 'blaOXA-66', '100.00', '100.00', 'ncbi', 'NG_049806.1', 'OXA-51 family carbapenem-hydrolyzing class D beta-lactamase OXA-66', 'CARBAPENEM'], ['2320194', "aph(3'')-Ib", '98.31', '99.88', 'ncbi', 'NG_056002.2', "aminoglycoside O-phosphotransferase APH(3'')-Ib", 'STREPTOMYCIN'], ['2320194', 'aph(6)-Id', '100.00', '100.00', 'ncbi', 'NG_047464.1', 'aminoglycoside O-phosphotransferase APH(6)-Id', 'STREPTOMYCIN'], ['2320194', 'tet(B)', '99.50', '100.00', 'ncbi', 'NG_048161.1', 'tetracycline efflux MFS transporter Tet(B)', 'TETRACYCLINE'], ['2320194', 'blaADC-30', '100.00', '100.00', 'ncbi', 'NG_048652.1', 'class C extended-spectrum beta-lactamase ADC-30', 'CEPHALOSPORIN'], ['2320194', 'blaOXA-66', '100.00', '100.00', 'ncbi', 'NG_049806.1', 'OXA-51 family carbapenem-hydrolyzing class D beta-lactamase OXA-66', 'CARBAPENEM'], ['2320194', 'mph(E)', '100.00', '100.00', 'ncbi', 'NG_064660.1', "Mph(E) family macrolide 2'-phosphotransferase", 'MACROLIDE'], ['2320194', 'msr(E)', '100.00', '99.93', 'ncbi', 'NG_048007.1', 'ABC-F type ribosomal protection protein Msr(E)', 'MACROLIDE'], ['2320194', "aac(6')-Ip", '100.00', '99.66', 'ncbi', 'NG_047307.2', "aminoglycoside 6'-N-acetyltransferase AAC(6')-Ip", 'AMINOGLYCOSIDE'], ['2320194', 'sul2', '100.00', '100.00', 'ncbi', 'NG_051852.1', 'sulfonamide-resistant dihydropteroate synthase Sul2', 'SULFONAMIDE'], ['2320194', 'blaOXA-72', '100.00', '100.00', 'ncbi', 'NG_049813.1', 'OXA-24 family carbapenem-hydrolyzing class D beta-lactamase OXA-72', 'CARBAPENEM'], ['2320194', "ant(3'')-IIa", '100.00', '98.61', 'ncbi', 'NG_054646.1', "aminoglycoside nucleotidyltransferase ANT(3'')-IIa", 'SPECTINOMYCIN;STREPTOMYCIN'], ['2320193', 'blaADC-30', '100.00', '100.00', 'ncbi', 'NG_048652.1', 'class C extended-spectrum beta-lactamase ADC-30', 'CEPHALOSPORIN'], ['2320193', "ant(3'')-IIa", '100.00', '98.61', 'ncbi', 'NG_054646.1', "aminoglycoside nucleotidyltransferase ANT(3'')-IIa", 'SPECTINOMYCIN;STREPTOMYCIN'], ['2320193', "aph(3'')-Ib", '98.31', '99.88', 'ncbi', 'NG_056002.2', "aminoglycoside O-phosphotransferase APH(3'')-Ib", 'STREPTOMYCIN'], ['2320193', 'aph(6)-Id', '100.00', '100.00', 'ncbi', 'NG_047464.1', 'aminoglycoside O-phosphotransferase APH(6)-Id', 'STREPTOMYCIN'], ['2320193', 'tet(B)', '99.50', '100.00', 'ncbi', 'NG_048161.1', 'tetracycline efflux MFS transporter Tet(B)', 'TETRACYCLINE'], ['2320193', 'blaOXA-66', '100.00', '100.00', 'ncbi', 'NG_049806.1', 'OXA-51 family carbapenem-hydrolyzing class D beta-lactamase OXA-66', 'CARBAPENEM'], ['2320193', 'blaOXA-72', '100.00', '100.00', 'ncbi', 'NG_049813.1', 'OXA-24 family carbapenem-hydrolyzing class D beta-lactamase OXA-72', 'CARBAPENEM'], ['2320193', "aac(6')-Ip", '100.00', '99.66', 'ncbi', 'NG_047307.2', "aminoglycoside 6'-N-acetyltransferase AAC(6')-Ip", 'AMINOGLYCOSIDE'], ['2320193', 'sul2', '100.00', '100.00', 'ncbi', 'NG_051852.1', 'sulfonamide-resistant dihydropteroate synthase Sul2', 'SULFONAMIDE'], ['2320193', 'mph(E)', '86.55', '100.00', 'ncbi', 'NG_064660.1', "Mph(E) family macrolide 2'-phosphotransferase", 'MACROLIDE']]
-        #df_table_col_query = "(" + ", ".join(self.df.columns.astype(str).tolist()) + ")"
-        #print(df_demo_lst)
-        self.write_query_genes = (" ").join(self.write_query_genes)
-   
-        self.db_handler.lst_ptr_push(df_lst=df_demo_lst, query=self.write_query_genes)
-    
+        #self.log.write_log("database_push","Done!`")
 
     def setup_db(self):
         self.db_handler = ms_sql_handler(self)
         self.db_handler.establish_db()
     
+    #will need a function for resistance table push
+    def database_push_genes(self):
+        self.setup_db()
+        df_demo_lst = self.genes_df.values.astype(str).tolist()
+        self.write_query_genes = (" ").join(self.write_query_genes)
+   
+        self.db_handler.lst_ptr_push(df_lst=df_demo_lst, query=self.write_query_genes)
+    
 
 
 if __name__ == "__main__":
     
-    import_demo = demographics_import("/home/ssh_user/Documents/GitHub/CRAB_Analysis")
-    sample_hsn = import_demo.get_lims_demographics(['2434975','2445821','2468507','2488768','2492075','2506355','2510743','2527973'],"111323","/home/ssh_user/WGS_Drive/CRAB_WGS_Sequencing")
+    import_demo = demographics_import("/epi/home/ssh_user/Documents/GitHub/CRAB_Analysis")
+    sample_hsn = import_demo.get_lims_demographics(['2434975','2445821','2468507','2488768','2492075','2506355','2510743','2527973'],"111323","/epi/home/ssh_user/WGS_Drive/CRAB_WGS_Sequencing")
     
-    #import_demo.database_push()
-    #import_demo.get_lims_demographics(["1"],'020323','PATH')
-    #import_demo.create_genes_df("{'2296669': [['2296669', 'blaADC-30', '100.00', '100.00', 'ncbi', 'NG_048652.1', 'class C extended-spectrum beta-lactamase ADC-30', 'CEPHALOSPORIN'], ['2296669', 'tet(B)', '99.50', '100.00', 'ncbi', 'NG_048161.1', 'tetracycline efflux MFS transporter Tet(B)', 'TETRACYCLINE'], ['2296669', 'aph(6)-Id', '100.00', '100.00', 'ncbi', 'NG_047464.1', 'aminoglycoside O-phosphotransferase APH(6)-Id', 'STREPTOMYCIN'], ['2296669', 'aph(3'')-Ib', '98.31', '99.88', 'ncbi', 'NG_056002.2', 'aminoglycoside O-phosphotransferase APH(3'')-Ib', 'STREPTOMYCIN'], ['2296669', 'blaOXA-66', '100.00', '100.00', 'ncbi', 'NG_049806.1', 'OXA-51 family carbapenem-hydrolyzing class D beta-lactamase OXA-66', 'CARBAPENEM'], ['2296669', 'sul2', '100.00', '100.00', 'ncbi', 'NG_051852.1', 'sulfonamide-resistant dihydropteroate synthase Sul2', 'SULFONAMIDE'], ['2296669', 'ant(3'')-IIa', '100.00', '98.61', 'ncbi', 'NG_054646.1', 'aminoglycoside nucleotidyltransferase ANT(3'')-IIa', 'SPECTINOMYCIN;STREPTOMYCIN'], ['2296669', 'mph(E)', '100.00', '100.00', 'ncbi', 'NG_064660.1', 'Mph(E) family macrolide 2'-phosphotransferase', 'MACROLIDE'], ['2296669', 'msr(E)', '100.00', '100.00', 'ncbi', 'NG_048007.1', 'ABC-F type ribosomal protection protein Msr(E)', 'MACROLIDE'], ['2296669', 'aac(6')-Ip', '100.00', '99.66', 'ncbi', 'NG_047307.2', 'aminoglycoside 6'-N-acetyltransferase AAC(6')-Ip', 'AMINOGLYCOSIDE'], ['2296669', 'aac(3)-Ia', '100.00', '100.00', 'ncbi', 'NG_047234.1', 'aminoglycoside N-acetyltransferase AAC(3)-Ia', 'GENTAMICIN'], ['2296669', 'blaOXA-72', '100.00', '100.00', 'ncbi', 'NG_049813.1', 'OXA-24 family carbapenem-hydrolyzing class D beta-lactamase OXA-72', 'CARBAPENEM']]}")
-    #Desktop/CRAB_OUT/2296669_manualy/scaffolds.fasta 
         
