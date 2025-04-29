ff041c3d60bc552eaa2cb1943b0634c618cf8d04
Ashita  D Jawali <113736983+Ashita2619@users.noreply.github.com>
2025-04-29 15:26:46 -0500

    Update WF_1_Launcher.py

--- scripts/Phoenix_Launcher/WF_1_Launcher.py
+++ scripts/Phoenix_Launcher/WF_1_Launcher.py
@@ -43,5 +43,5 @@ def WF_3_create_report(samps,run_d,pdf_path,resource_p,genes,mlst_d):
 
 if __name__ == "__main__":
 
-    WF_1_Launch_Phoenix("/home/ssh_user/WGS_Drive/Phoenix/sample_fastq/032323","/home/ssh_user/WGS_Drive/Phoenix/SampleSheet","032323","/home/ssh_user/WGS_Drive/Phoenix/Output","/home/ssh_user/Documents/GitHub/phoenix","/media/ssh_user/Data_Drive/Minikraken2DB")
+    WF_1_Launch_Phoenix("/epi/home/ssh_user/WGS_Drive/Phoenix/sample_fastq/032323","/epi/home/ssh_user/WGS_Drive/Phoenix/SampleSheet","032323","/epi/home/ssh_user/WGS_Drive/Phoenix/Output","/epi/home/ssh_user/Documents/GitHub/phoenix","/media/ssh_user/Data_Drive/Minikraken2DB")
     
