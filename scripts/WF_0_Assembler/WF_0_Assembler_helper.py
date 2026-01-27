cc23990c0e945c7239f13b29bff126ad840c3c41
Ashita  D Jawali <113736983+Ashita2619@users.noreply.github.com>
2026-01-27 08:37:10 -0600

    Update WF_0_Assembler_helper.py
    Changed the working directory.

--- scripts/WF_0_Assembler/WF_0_Assembler_helper.py
+++ scripts/WF_0_Assembler/WF_0_Assembler_helper.py
@@ -18,7 +18,7 @@ def data_pre_processor(path_to_reads,sample_d):
         if not(os.path.exists(path_to_reads+"/fastp/"+sample)):
             os.mkdir(path_to_reads+"fastp/"+sample)
         
-        subprocess.run(". $CONDA_PREFIX/home/ssh_user/mambaforge/etc/profile.d/conda.sh && conda activate CRAB && fastp -i "+path_to_reads+"/"+sample_d[sample][0]+" -o "+path_to_reads+"/fastp/"+sample+"/"+sample+"_R1_fp.fastq.gz -I "+path_to_reads+"/"+sample_d[sample][1]+" -O "+path_to_reads+"/fastp/"+sample+"/"+sample+"_R2_fp.fastq.gz -h "+path_to_reads+"/"+sample+"/"+sample+".html",shell=True)
+        subprocess.run(". $CONDA_PREFIX/epi/home/ashita.jawali@kdhe.state.ks.us/mambaforge/etc/profile.d/conda.sh && conda activate CRAB && fastp -i "+path_to_reads+"/"+sample_d[sample][0]+" -o "+path_to_reads+"/fastp/"+sample+"/"+sample+"_R1_fp.fastq.gz -I "+path_to_reads+"/"+sample_d[sample][1]+" -O "+path_to_reads+"/fastp/"+sample+"/"+sample+"_R2_fp.fastq.gz -h "+path_to_reads+"/"+sample+"/"+sample+".html",shell=True)
         sample_d[sample][0]= sample+"_R1_fp.fastq.gz"
         sample_d[sample][1]= sample+"_R2_fp.fastq.gz"
  
@@ -35,7 +35,7 @@ def tree_data_pre_processor(path_to_reads,sample_d):
         if not(os.path.exists(path_to_reads+"/tree/"+sample)):
             os.mkdir(path_to_reads+"/tree/"+sample)
         
-        #subprocess.run(". $CONDA_PREFIX/home/ssh_user/mambaforge/etc/profile.d/conda.sh && conda activate CRAB && fastp -i "+path_to_reads+"/"+sample_d[sample][0]+" -o "+path_to_reads+"/fastp/"+sample+"/"+sample+"_R1_fp.fastq.gz -I "+path_to_reads+"/"+sample_d[sample][1]+" -O "+path_to_reads+"/fastp/"+sample+"/"+sample+"_R2_fp.fastq.gz -h "+path_to_reads+"/"+sample+"/"+sample+".html",shell=True)
+        #subprocess.run(". $CONDA_PREFIX/epi/home/ashita.jawali@kdhe.state.ks.us/mambaforge/etc/profile.d/conda.sh && conda activate CRAB && fastp -i "+path_to_reads+"/"+sample_d[sample][0]+" -o "+path_to_reads+"/fastp/"+sample+"/"+sample+"_R1_fp.fastq.gz -I "+path_to_reads+"/"+sample_d[sample][1]+" -O "+path_to_reads+"/fastp/"+sample+"/"+sample+"_R2_fp.fastq.gz -h "+path_to_reads+"/"+sample+"/"+sample+".html",shell=True)
         subprocess.run("mv "+path_to_reads+"/"+sample_d[sample][0]+" "+path_to_reads+"/tree/"+sample+"/"+sample+"_R1_t.fastq.gz",shell=True)
         subprocess.run("mv "+path_to_reads+"/"+sample_d[sample][1]+" "+path_to_reads+"/tree/"+sample+"/"+sample+"_R2_t.fastq.gz",shell=True)
         sample_d[sample][0]= sample+"_R1_t.fastq.gz"
@@ -51,7 +51,7 @@ def run_assembler(resource_path,reads_path, sample_d, output_dir,runD):
             for sample in sample_d:
 
                 os.mkdir(output_dir+"/"+sample)
-                #subprocess.run("/home/ssh_user/miniconda3/bin/python3.9 /Users/adrian/Desktop/SPAdes-3.15.5-Darwin/bin/spades.py --isolate -o"+output_dir+"/"+sample+" --pe1-1 "+reads_path+"/fastp/"+sample+"/"+sample_d[sample][0]+" --pe1-2 "+reads_path+"/fastp/"+sample+"/"+sample_d[sample][1], shell=True)
+                #subprocess.run("/epi/home/ashita.jawali@kdhe.state.ks.us/miniconda3/bin/python3.9 /Users/adrian/Desktop/SPAdes-3.15.5-Darwin/bin/spades.py --isolate -o"+output_dir+"/"+sample+" --pe1-1 "+reads_path+"/fastp/"+sample+"/"+sample_d[sample][0]+" --pe1-2 "+reads_path+"/fastp/"+sample+"/"+sample_d[sample][1], shell=True)
                 subprocess.run("python3 "+resource_path+"/resources/SPAdes-3.15.5-Linux/bin/spades.py --isolate -o "+output_dir+"/"+sample+" --pe1-1 "+reads_path+"/fastp/"+sample+"/"+sample_d[sample][0]+" --pe1-2 "+reads_path+"/fastp/"+sample+"/"+sample_d[sample][1], shell=True)
             
 
@@ -97,8 +97,8 @@ def check_assembly_qual(resource_path,path_to_fasta,qual_outputdir,samples,runD)
     #should also read in json file and store data
     for sample in samples:
         #os.mkdir(qual_outputdir+"/"+sample)
-        #print(". $CONDA_PREFIX/home/ssh_user/mambaforge/etc/profile.d/conda.sh && conda activate BUSCO_env && busco -i "+path_to_fasta+"/"+runD+"/"+sample+"/scaffolds.fasta -l "+resource_path+"/resources/busco/bacteria_odb10/ -o "+sample+"_busco --out_path "+qual_outputdir+" -m genome --offline")
-        subprocess.run("bash -c 'source /home/ssh_user/miniconda3/bin/activate BUSCO && busco -i "+path_to_fasta+"/"+runD+"/"+sample+"/scaffolds.fasta -l "+resource_path+"/resources/busco/bacteria_odb10/ -o "+sample+"_busco --out_path "+qual_outputdir+"/ -m genome --offline'", shell=True)
+        #print(". $CONDA_PREFIX/epi/home/ashita.jawali@kdhe.state.ks.us/mambaforge/etc/profile.d/conda.sh && conda activate BUSCO_env && busco -i "+path_to_fasta+"/"+runD+"/"+sample+"/scaffolds.fasta -l "+resource_path+"/resources/busco/bacteria_odb10/ -o "+sample+"_busco --out_path "+qual_outputdir+" -m genome --offline")
+        subprocess.run("bash -c 'source /epi/home/ashita.jawali@kdhe.state.ks.us/miniconda3/bin/activate BUSCO && busco -i "+path_to_fasta+"/"+runD+"/"+sample+"/scaffolds.fasta -l "+resource_path+"/resources/busco/bacteria_odb10/ -o "+sample+"_busco --out_path "+qual_outputdir+"/ -m genome --offline'", shell=True)
         
         #reading in json for stats
         temp = open(qual_outputdir+"/"+sample+"_busco"+"/short_summary.specific.bacteria_odb10."+sample+"_busco.json","r")
